ff4c5dd8aef378914817a237aaf64f373afd96cf
Ksenia <vladislav.bandana@gmail.com>
2025-04-05 15:55:25 +0300

    Update

---
+++ OOP-practiceBerestovskiy/src/task1/code/Main1.java
@@ -0,0 +1,9 @@
+package task1.code;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Берестовський Владислав, 35 група, 1 підгрупа");
+    }
+}
+
